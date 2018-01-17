e5e09b108eb4aadf778c84958b0ad1e650e8248c
Emelie Sidesiö <esidesio@gmail.com>
2018-01-17 15:02:57 +0100

    Created User component, Browserrouter added

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -1,51 +1,17 @@
 import React from "react"
-import { BrowserRouter, Route, Link } from "react-router-dom"
-
-import Form from "components/form/form"
-import Category from "components/category/category"
-import Threads from "components/threads/threads"
+import { BrowserRouter, Route } from "react-router-dom"
+import Users from "pages/users/users"
 import Admin from "pages/admin/admin"
-
-const newQuestionForm = [
-  { inputName: "title", inputType: "text", inputPlaceHolder: "Question title" },
-  { inputName: "text", inputType: "text", inputPlaceHolder: "Write your question here:" },
-  { inputName: "sender", inputType: "text", inputPlaceHolder: "Your name here:" }
-]
-
-const responseForm = [
-  { inputName: "text", inputType: "text", inputPlaceHolder: "Write your question here:" },
-  { inputName: "sender", inputType: "text", inputPlaceHolder: "Your name here:" }
-]
 
 class App extends React.Component {
 
   render() {
     return (
       <BrowserRouter>
-      <div className="forum-page">
-        <div className="forum-top">
-          <h1>MongoDB Forum</h1>
+        <div>
+          <Users />
         </div>
-        <div className="forum-body">
-          <div className="question-container">
-            <Category />
-            <h2>Latest questions</h2>
-            <Threads
-              formInput={responseForm}
-              type="response" />
-          </div>
-          <div className="question-form">
-            <h2>Post a question</h2>
-            <Form
-              formInput={newQuestionForm}
-              type="newQuestion" />
-          </div>
-
-          <Route path="/admin" component={Admin} />
-
-          <Link to="/admin">Admin</Link>
-          </div>
-        </div>
+        <Route path="/admin" component={Admin} />
       </BrowserRouter>
     )
   }
