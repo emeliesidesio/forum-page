f390fae9fd5c81e953496ccdc80a53cb2c143ab2
Hilda <hilda.sandberg@gmail.com>
2018-01-17 16:35:09 +0100

    category and form with conditionally rendering

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -1,18 +1,19 @@
 import React from "react"
 // import { BrowserRouter, Route } from "react-router-dom"
 import Users from "pages/users/users"
-// import Admin from "pages/admin/admin"
+import Admin from "pages/admin/admin"
 
 class App extends React.Component {
 
   render() {
     return (
-      <BrowserRouter>
-        <div>
-          <Route exact path="/" component={Users} />
-          <Route exact path="/admin" component={Admin} />
-        </div>
-      </BrowserRouter>
+      // <BrowserRouter>
+      //   <div>
+      //     <Route exact path="/" component={Users} />
+      //     <Route exact path="/admin" component={Admin} />
+      //   </div>
+      // </BrowserRouter>
+    <Users />
     )
   }
 
